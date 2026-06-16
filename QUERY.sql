61c0118f92e5cffad7e33da8ffe4d1b05787d99c
Tanvir Hossan Ramim <132383235+Tanvir-Ramim@users.noreply.github.com>
2026-06-16 18:12:55 +0600

    all type , check add

--- QUERY.sql
+++ QUERY.sql
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 -- =========================================================================
 -- SYSTEM: Football Ticket Booking System Database Setup Template
 -- DESCRIPTION: Pseudo-DDL Template for Table Creation & Data Insertion
@@ -6,19 +13,19 @@
 -- =========================================================================
 
 -- DROP TABLES IF THEY ALREADY EXIST TO PREVENT CONFLICTS
-DROP TABLE IF EXISTS Bookings;
-DROP TABLE IF EXISTS Matches;
-DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Bookingg
+DROP TABLE IF EXISTS Matches
+DROP TABLE IF EXISTS Users
 
 -- =========================================================================
 -- 1. CREATE USERS TABLE
 -- =========================================================================
 CREATE TABLE Users (
-    user_id TYPE,
-    full_name TYPE,
-    email TYPE,
-    role TYPE,
-    phone_number TYPE,
+    user_id serial primary key,
+    full_name varchar(100) not null ,
+    email varchar unique not null,
+    role varchar(30) check(role IN ('Football Fan', 'Ticket Manager')),
+    phone_number varchar(20)
     
     -- Write your constraint to make 'user_id' the Primary Key
     -- Write your constraint to ensure 'email' values are never duplicated
@@ -29,11 +36,11 @@ CREATE TABLE Users (
 -- 2. CREATE MATCHES TABLE
 -- =========================================================================
 CREATE TABLE Matches (
-    match_id TYPE,
-    fixture TYPE,
-    tournament_category TYPE,
-    base_ticket_price TYPE,
-    match_status TYPE,
+    match_id serial primary key,
+    fixture varchar(200) not null,
+    tournament_category varchar(50) not null,
+    base_ticket_price decimal(10,2) check(base_ticket_price>=0),
+    match_status varchar(20) check(match_status in ('Available','Selling Fast','Sold Out','Postponed'))
     
     -- Write your constraint to make 'match_id' the Primary Key
     -- Write your check constraint to prevent negative ticket prices
@@ -44,19 +51,19 @@ CREATE TABLE Matches (
 -- 3. CREATE BOOKINGS TABLE
 -- =========================================================================
 CREATE TABLE Bookings (
-    booking_id TYPE,
-    user_id TYPE,
-    match_id TYPE,
-    seat_number TYPE,
-    payment_status TYPE,
-    total_cost TYPE,
+    booking_id serial primary key,
+    user_id int references Users(user_id),
+    match_id int references Matches(match_id),
+    seat_number varchar(20),
+    payment_status varchar(20) check (payment_status in ('Pending','Confirmed', 'Cancelled','Refunded')),
+    total_cost decimal(10,2) check (total_cost >=0)
     
     -- Write your constraint to make 'booking_id' the Primary Key
     -- Write your Foreign Key constraint linking 'user_id' to the Users table
     -- Write your Foreign Key constraint linking 'match_id' to the Matches table
     -- Write your check constraint to ensure 'total_cost' is non-negative
     -- Write your check constraint to restrict 'payment_status' values
-);
+)
 
 
 -- =========================================================================
@@ -66,7 +73,7 @@ INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
 (1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
 (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
-(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL)
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO MATCHES
@@ -76,7 +83,7 @@ INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, 
 (102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
 (103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
 (104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
-(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available')
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO BOOKINGS
@@ -86,4 +93,6 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (502, 1, 102, 'B-04', 'Confirmed', 120.00),
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
-(505, 3, 102, 'C-20', 'Pending', 120.00);
+(505, 3, 102, 'C-20', 'Pending', 120.00)
+
+-- alter table Users rename column  phone_numbe to phone_number
